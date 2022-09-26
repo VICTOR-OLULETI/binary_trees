c5a1de225235b0a3a674aa75fa9d4e12db4acb01
VICTOR-OLULETI <oluletiv@gmail.com>
2022-09-26 21:30:59 +0100

    checks if binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,6 +3,24 @@
 #include "binary_trees.h"
 
 /**
+ * tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ *
+ * Return: size
+ */
+size_t tree_size(const binary_tree_t *tree)
+{
+	const binary_tree_t *root = tree;
+	size_t h1, h2;
+
+	if (root == NULL)
+		return (0);
+
+	h1 = tree_size(root->left);
+	h2 = tree_size(root->right);
+	return (h1 + h2 + 1);
+}
+/**
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: pointer to the root node of the tree to check
  *
@@ -11,15 +29,15 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	const binary_tree_t *root = tree;
+	int h1;
 
 	if (root == NULL)
 		return (0);
-	if (root->parent == NULL)
-		return (0);
-	if ((root->left != NULL && root->right != NULL) ||
-			(root->left == NULL && root->right == NULL))
+	if (root != NULL)
 	{
-		return (1);
+		h1 = tree_size(root);
+		if (h1 % 2 != 0)
+			return (1);
 	}
 	return (0);
 }
